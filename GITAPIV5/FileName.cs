a091d3856a25d6f0732ba54e8bf6d2ff7357bc08
Julian Mora <julianmr79@hotmail.com>
2024-12-06 21:06:00 -0500

    mi segundo commit

---
+++ GITAPIV5/FileName.cs
@@ -0,0 +1,7 @@
+﻿namespace GITAPIV5
+{
+    public class FileName
+    {
+        public int MyProperty { get; set; }
+    }
+}
--- GITAPIV5/obj/Debug/net8.0/GITAPIV5.AssemblyInfo.cs
+++ GITAPIV5/obj/Debug/net8.0/GITAPIV5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GITAPIV5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb8e3d3189c5bb08c97321235d1db35fd049cec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GITAPIV5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GITAPIV5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
